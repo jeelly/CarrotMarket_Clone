5e0e1d22f011f2cb3a6e59ef0d7ecfc06e9297de
jeelly <khk3159@naver.com>
2022-06-17 12:28:10 +0900

    GlobalStyles 만듬 #1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './css/App.css';
-
 //Router
 import { BrowserRouter } from 'react-router-dom';
 //Sub
 import Router from './page/Router';
+import GlobalStyles from './css/GlobalStyles'
 
 function App() {
   return (
     <BrowserRouter>
       <Router />
+      <GlobalStyles/>
     </BrowserRouter>
   );
 }
