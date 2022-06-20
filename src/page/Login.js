43138946ff07497020fb6f7f894b4f2c89f9a422
jrl103 <jrl103@naver.com>
2022-06-21 03:17:55 +0900

    로그인, 회원가입 벨리데이션 수정 #2, #3

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import styled from "styled-components";
 import { useDispatch } from "react-redux";
 import { loginUserDB } from "../redux/modules/userSlice";
@@ -6,43 +6,71 @@ import { loginUserDB } from "../redux/modules/userSlice";
 const Login = () => {
   const dispatch = useDispatch();
 
-  const id_ref = React.useRef(null);
-  const pwd_ref = React.useRef(null);
+  const usernameRef = React.useRef(null);
+  const passwordRef = React.useRef(null);
 
-  const emailCheck = (email) => {
-    let _reg =
-      /^[0-9a-zA-Z]([-_.0-9a-zA-Z])*@[0-9a-zA-Z]([-_.0-9a-zA-Z])*.([a-zA-Z])*/;
-    //이메일 형식으로!
-    return _reg.test(email);
-  };
+  //아이디, 이메일, 비밀번호, 비밀번호 확인
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  // 유효성 검사
+  const [isUsername, setIsUsername] = React.useState(false);
+  const [isPassword, setIsPassword] = React.useState(false);
+
+  //오류메시지 상태저장
+  const [usernameMessage, setUsernameMessage] = React.useState("");
+  const [passwordMessage, setPasswordMessage] = React.useState("");
 
   const login = () => {
-    if (id_ref.current.value === "" || pwd_ref.current.value === "") {
-      window.alert("아이디와 비밀번호를 입력하세요!");
-      return;
-    }
-    if (!emailCheck(id_ref.current.value)) {
-      window.alert("이메일 형식이 맞지 않습니다!");
-      return;
-    } else {
-    }
     let users = {
-      username: id_ref.current.value,
-      password: pwd_ref.current.value,
+      username: usernameRef.current.value,
+      password: passwordRef.current.value,
     };
+    //dispatch 할 때 users 데이터와 close 함수 전달 (함수전달 가능, 함수 전달 할 땐 괄호 없어야함.)
     dispatch(loginUserDB(users));
   };
 
-  // 버튼 비활성화
-  const [username, setUsername] = React.useState("");
-  const [pwd, setPwd] = React.useState("");
-  const checkUsername = (e) => {
-    setUsername(e.target.value);
-  };
-  const checkPwd = (e) => {
-    setPwd(e.target.value);
-  };
+// username
+const onChangeUsername = useCallback((e) => {
+  
+  const usernameRegex =
+    /([\w-.]+)@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.)|(([\w-]+.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(]?)$/;
+  const usernameCurrent = usernameRef.current.value;
+  setUsername(usernameCurrent);
 
+  if (!usernameRegex.test(usernameCurrent)) {
+    setUsernameMessage("이메일 형식을 다시 한번 확인해 주세요.");
+    setIsUsername(false);
+  } else {
+    setUsernameMessage("알맞게 작성되었습니다 :)");
+    setIsUsername(true);
+  }
+}, []);
+
+  // 비밀번호
+  const onChangePassword = useCallback((e) => {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z])(?=.*[@$!%*#?&])[0-9a-zA-Z@$!%*#?&]{3,10}$/;
+
+    //비밀번호는 3 ~ 10자 영문, 숫자 및 특수문자조합으로
+    const passwordCurrent = passwordRef.current.value;
+    setPassword(passwordCurrent);
+
+    if (!passwordRegex.test(passwordCurrent)) {
+      setPasswordMessage("비밀번호는 3 ~ 10자 영문, 숫자 및 특수문자조합으로");
+      setIsPassword(false);
+    } else {
+      setPasswordMessage("알맞게 작성되었습니다 :)");
+      setIsPassword(true);
+    }
+  }, []);
+
+  // 엔터키 설정
+  const onKeyPress = (e) => {
+    if (e.key === "Enter") {
+      login();
+    }
+  };
+  
   return (
     <Wrap>
       <h1>로그인</h1>
@@ -52,32 +80,47 @@ const Login = () => {
           <br />
           <input
             type="email"
-            ref={id_ref}
+            ref={usernameRef}
             // 버튼 비활성화
-            onChange={checkUsername}
+            onChange={onChangeUsername}
             required
           />
+          <Pone>
+            {username.length > 0 && (
+              <span className={`message ${isUsername ? "success" : "error"}`}>
+                {usernameMessage}
+              </span>
+            )}
+          </Pone>
         </IdBox>
-        <PwdBox>
+        <IdBox>
           <p>비밀번호</p>
           <br />
           <input
             type="password"
-            ref={pwd_ref}
+            ref={passwordRef}
             // 버튼 비활성화
-            onChange={checkPwd}
+            onChange={onChangePassword}
+            onKeyPress={onKeyPress}
             required
           />
-        </PwdBox>
+          <Pone>
+            {password.length > 0 && (
+              <span className={`message ${isPassword ? "success" : "error"}`}>
+                {passwordMessage}
+              </span>
+            )}
+          </Pone>
+        </IdBox>
       </div>
       <Button
         onClick={() => {
           login();
         }}
         // 버튼 비활성화
-        disabled={!username || !pwd}
+        disabled={!(isUsername && isPassword && username && password)}
       >
-        로그인하기
+        로그인
       </Button>
     </Wrap>
   );
@@ -87,7 +130,10 @@ const Wrap = styled.div``;
 const IdBox = styled.div``;
 const PwdBox = styled.div``;
 const Button = styled.button`
-background-color: ${(props) => (props.disabled ? "#f8cbac" : "#ff8a3a")};
+  background-color: ${(props) => (props.disabled ? "#f8cbac" : "#ff8a3a")};
 `;
-
+const Pone = styled.p`
+  color: grey;
+  font-size: 13px;
+`;
 export default Login;
