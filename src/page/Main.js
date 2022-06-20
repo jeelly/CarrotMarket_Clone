7998e3a064b682d0db37ddc63a25f367bef92054
jeelly <khk3159@naver.com>
2022-06-20 21:32:53 +0900

    메인페이지 Post버튼 구현

--- src/page/Main.js
+++ src/page/Main.js
@@ -4,12 +4,15 @@ import styled from "styled-components";
 //Subpage
 import VisualArea from '../components/main/VisualArea'
 import ContentArea from '../components/main/ContentArea';
+//router
+import { Link } from 'react-router-dom';
 
 const Main = () => {
     return (
       <Container>
         <VisualArea></VisualArea>
         <ContentArea></ContentArea>
+        <PostBtn to="/post">+</PostBtn>
       </Container>
     );
 };
@@ -17,3 +20,18 @@ const Main = () => {
 export default Main;
 
 const Container = styled.div``;
+const PostBtn = styled(Link)`
+  width:50px;
+  height:50px;
+  border-radius:50%;
+  position:fixed;
+  bottom:5vh;
+  right:2vw;
+  font-size:50px;
+  color:#eee;
+  line-height:40px;
+  background-color:#fa6616;
+  &:hover {
+    background-color:rgba(250, 102, 22,0.5);
+  }
+`
